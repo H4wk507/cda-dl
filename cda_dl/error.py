8954afc19c59e21a70202e4b7100426c30626758
H4wk <quasand89@gmail.com>
2023-09-12 21:24:03 +0200

    add missing property

--- cda_dl/error.py
+++ cda_dl/error.py
@@ -21,9 +21,10 @@ class LoginRequiredError(Exception):
 class HTTPError(Exception):
     def __init__(self, message: str, status_code: int):
         super().__init__(message)
+        self.message = message
         self.status_code = status_code
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.message
 
 
