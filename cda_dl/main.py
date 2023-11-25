6d79c0576467fbb0b0447271bba631ea275823d1
H4wk <quasand89@gmail.com>
2023-11-25 23:33:13 +0100

    update release script

--- cda_dl/main.py
+++ cda_dl/main.py
@@ -4,9 +4,9 @@ import sys
 from typing import Sequence
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from importlib.metadata import version
 
 from cda_dl.downloader import Downloader
+from cda_dl.version import __version__
 
 
 class CustomHelpFormatter(argparse.HelpFormatter):
@@ -38,7 +38,7 @@ def parse_args(argv: Sequence[str] | None) -> argparse.Namespace:
     parser.add_argument(
         "--version",
         action="version",
-        version=f"{parser.prog} {version(parser.prog)}",
+        version=f"{parser.prog} {__version__}",
         help="Wyświetl wersję programu",
     )
     parser.add_argument(
